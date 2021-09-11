14805a5dbbffab1861d9f3336bb8ea0bc509f851
Sergio Gómez <decano@gmail.com>
2021-09-11 20:21:24 +0200

    feat(web): implement todo

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,10 @@
+import axios from 'axios';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 
 import App from './app/app';
+
+axios.defaults.baseURL = 'http://localhost:3333';
 
 ReactDOM.render(
   <StrictMode>
